a23bc2ea3add1c7e23e56936c0cb9e99588b43e8
James DiTucci <dituccjv@rose-hulman.edu>
2016-11-06 17:47:16 -0500

    merge issue

--- project/http/boneServer.js
+++ project/http/boneServer.js
@@ -5,6 +5,7 @@
 // buttonBox, ioPlot, realtimeDemo
 "use strict";
 
+
 var port = 9090, // Port to listen on
     bus = '/dev/i2c-2',
     busNum = 2,     // i2c bus number
@@ -39,6 +40,9 @@ function initIO() {
     b.pinMode('P9_42', b.INPUT);
     b.pinMode('P9_41', b.INPUT);
     b.pinMode(pwm,     b.ANALOG_OUTPUT);    // PWM
+
+
+
     
     // Initialize pwm
 }
